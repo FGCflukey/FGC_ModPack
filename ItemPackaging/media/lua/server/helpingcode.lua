5051b8927d12582a4522376b6adb2a6e0039565a
Flukester69 <59144393+Flukester69@users.noreply.github.com>
2022-06-18 22:31:09 -0400

    ItemPackaging

---
+++ ItemPackaging/media/lua/server/helpingcode.lua
@@ -0,0 +1,12 @@
+PackageRecipes = {}
+
+function PackageRecipes.OnTestPacking(item, result)
+
+    if item == nil then return false end
+--    if item:getType() == "BlowTorch" and item:getUsedDelta() == 1 then
+	  if item:getUsedDelta() == 1 then
+        return true
+    else
+        return false
+    end
+end
