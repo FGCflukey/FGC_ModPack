562a05be61a7443f5edb09c4ece9b5cbc7efead6
Uncle-Griz <107781050+Uncle-Griz@users.noreply.github.com>
2022-06-24 19:06:22 -0400

    Update FlukeyCoords.lua
    Added a catch for situations when a character can not 'getsquare'  such as the trailer interior mod

--- QOL Fixers/media/lua/client/ISUI/FlukeyCoords.lua
+++ QOL Fixers/media/lua/client/ISUI/FlukeyCoords.lua
@@ -7,6 +7,9 @@ function ISEquippedItem:prerender()
 		local maxY = getCore():getScreenHeight();
 		local maxX = getCore():getScreenWidth();
 		
-		self:drawText( "Coords: "..self.chr:getSquare():getX().."/"..self.chr:getSquare():getY(), maxX-300, maxY-60, 0.8, 0.8, 0.8, 1, UIFont.NewMedium)
+		local square = self.chr:getSquare() --this should catch any situation where a character can not 'getSquare'
+		if square then 
+			self:drawText( "Coords: "..square:getX().."/"..square:getY(), maxX-300, maxY-60, 0.8, 0.8, 0.8, 1, UIFont.NewMedium)
+		end
 	end
 end
