f533578ad88aaf3a93bf32b33130fb37aeb8bbd2
FGCFlukey <tomvalenz@hotmail.com>
2022-07-16 12:25:25 -0400

    Update helpingcode.lua
    fixing typo.. happens when u re-edit just before bed.

--- ItemPackaging/media/lua/server/helpingcode.lua
+++ ItemPackaging/media/lua/server/helpingcode.lua
@@ -4,7 +4,7 @@ function PackageRecipes.OnTestPacking(item, result)
 
     if item == nil then return false end
 --    if item:getType() == "BlowTorch" and item:getUsedDelta() == 1 then
-	  if item:Drainable() and item:getUsedDelta() == 1 then
+	  if item:isDrainable() and item:getUsedDelta() == 1 then
         return true
     else
         return false
