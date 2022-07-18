621d1b650deca5b13dac36bb38adde11de997123
Uncle-Griz <107781050+Uncle-Griz@users.noreply.github.com>
2022-07-17 22:25:25 -0400

    Update ValhallaNoTow.lua
    added allowance for players on 'allowed to maintain' vehicle list to tow claimed vehicles

--- QOL Fixers/media/lua/client/ISUI/ValhallaNoTow.lua
+++ QOL Fixers/media/lua/client/ISUI/ValhallaNoTow.lua
@@ -12,12 +12,12 @@ function ISAttachTrailerToVehicle:isValid()
 			if ownerA ~= nil or ownerB ~= nil then
 				--print( "keyword: somebody owns something")
 				
-				if ownerA and ownerA ~= self.character:getUsername() then
+				if (ownerA and ownerA ~= self.character:getUsername()) or Valhalla.VehicleClaims:playerCanMaintainVehicle(self.character, self.vehicleA) == false then
 					--print( "keyword: owner A tick")
 					return false
 				end
 				
-				if ownerB and ownerB ~= self.character:getUsername() then
+				if (ownerB and ownerB ~= self.character:getUsername()) or Valhalla.VehicleClaims:playerCanMaintainVehicle(self.character, self.vehicleB) == false then
 					--print( "keyword: owner B tick")
 					return false
 				end				
