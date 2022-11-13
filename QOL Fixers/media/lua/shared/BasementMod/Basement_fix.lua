89e963fcb975cd67ee2eef72152f3a1bf5b556eb
FGCFlukey <tomvalenz@hotmail.com>
2022-11-13 17:39:07 -0500

    basementfix
    basement fix by Griz

---
+++ QOL Fixers/media/lua/shared/BasementMod/Basement_fix.lua
@@ -0,0 +1,9 @@
+local oldProc = Basement.IsObjectHatch
+
+--- Check an object is a hatch
+---@return boolean
+function Basement.IsObjectHatch(obj)
+	if obj == nil then return false
+	else
+	return oldProc( obj)
+end
