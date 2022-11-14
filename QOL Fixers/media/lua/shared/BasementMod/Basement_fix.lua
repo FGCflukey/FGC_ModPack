c0813aa03d2d2db8929143a5391b0ffadae0c891
FGCFlukey <tomvalenz@hotmail.com>
2022-11-14 17:21:33 -0500

    remarking basement fix
    cuz its screwed.

--- QOL Fixers/media/lua/shared/BasementMod/Basement_fix.lua
+++ QOL Fixers/media/lua/shared/BasementMod/Basement_fix.lua
@@ -1,11 +1,11 @@
-local oldProc = Basement.IsObjectHatch
+-- local oldProc = Basement.IsObjectHatch()
 
 --- Check an object is a hatch
 ---@return boolean
-function Basement.IsObjectHatch(obj)
-	if obj == nil then 
-		return false
-	else
-		return oldProc( obj)
-	end
-end
+-- function Basement.IsObjectHatch(obj)
+-- 	if obj == nil then 
+-- 		return false
+--	else
+--		return oldProc( obj)
+--	end
+-- end
