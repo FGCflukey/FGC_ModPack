42247f0600b1dae59ca87aa3a2c193f70675b950
FGCFlukey <tomvalenz@hotmail.com>
2022-11-27 21:57:32 -0500

    HelicopterFastTravelSystem
    Mod to allow people to teleport to other cities
    - requires $500 for 1-way travel
    - must package money
    - must make travel request package
    
    Once complete can pick destination.

---
+++ HelicopterFastTravel/media/lua/client/fasttravel.lua
@@ -0,0 +1,83 @@
+FastTravel = {}
+
+-- Chopper Travel
+
+function travelToEkron()	-- Ekron
+	local player = getPlayer();
+	player:setX(7084);
+	player:setY(8314);
+	player:setZ(1);
+	player:setLx(player:getX());
+	player:setLy(player:getY());
+	player:setLz(player:getZ());
+end
+
+function travelToMuld()		-- Muldraugh
+	local player = getPlayer();
+	player:setX(10635);
+	player:setY(10405);
+	player:setZ(1);
+	player:setLx(player:getX());
+	player:setLy(player:getY());
+	player:setLz(player:getZ());
+end
+
+function travelToRCreek()	-- Raven Creek
+	local player = getPlayer();
+	player:setX(3783);
+	player:setY(11816);
+	player:setZ(1);
+	player:setLx(player:getX());
+	player:setLy(player:getY());
+	player:setLz(player:getZ());
+end
+
+function travelToLv()  -- Louisville
+	local player = getPlayer();
+	player:setX(12450);
+	player:setY(1600);
+	player:setZ(1);
+	player:setLx(player:getX());
+	player:setLy(player:getY());
+	player:setLz(player:getZ());
+end
+
+function travelToRW()	-- Rosewood
+	local player = getPlayer();
+	player:setX(8070);
+	player:setY(11730);
+	player:setZ(1);
+	player:setLx(player:getX());
+	player:setLy(player:getY());
+	player:setLz(player:getZ());
+end
+
+function travelToWPD()	-- West Point
+	local player = getPlayer();
+	player:setX(11903);
+	player:setY(6950);
+	player:setZ(1);
+	player:setLx(player:getX());
+	player:setLy(player:getY());
+	player:setLz(player:getZ());
+end
+
+function travelToRS()	-- Riverside
+	local player = getPlayer();
+	player:setX(6084);
+	player:setY(5257);
+	player:setZ(1);
+	player:setLx(player:getX());
+	player:setLy(player:getY());
+	player:setLz(player:getZ());
+end
+
+function travelToMR()	-- March Ridge
+	local player = getPlayer();
+	player:setX(10173);
+	player:setY(12677);
+	player:setZ(1);
+	player:setLx(player:getX());
+	player:setLy(player:getY());
+	player:setLz(player:getZ());
+end
