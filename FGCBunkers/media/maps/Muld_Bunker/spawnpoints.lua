03314db1106cfc3809d50e56c61154d25b9aca1a
Uncle-Griz <107781050+Uncle-Griz@users.noreply.github.com>
2023-01-25 20:17:30 -0500

    Initial push for FGC Bunkers map

---
+++ FGCBunkers/media/maps/Muld_Bunker/spawnpoints.lua
@@ -0,0 +1,22 @@
+function SpawnPoints()
+return {
+  constructionworker = {
+    { worldX = 34, worldY = 32, posX = 265, posY = 149, posZ = 1 }
+  },
+  fireofficer = {
+    { worldX = 34, worldY = 32, posX = 265, posY = 149, posZ = 1 }
+  },
+  parkranger = {
+    { worldX = 34, worldY = 32, posX = 265, posY = 149, posZ = 1 }
+  },
+  policeofficer = {
+    { worldX = 34, worldY = 32, posX = 265, posY = 149, posZ = 1 }
+  },
+  securityguard = {
+    { worldX = 34, worldY = 32, posX = 265, posY = 149, posZ = 1 }
+  },
+  unemployed = {
+    { worldX = 34, worldY = 32, posX = 265, posY = 149, posZ = 1 }
+  }
+}
+end
